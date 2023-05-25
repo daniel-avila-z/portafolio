597ed231d9e309c7784c41ca5b7111fa6f2167f2
Daniel Avila <roicesdaniel@gmail.com>
2023-05-25 17:57:43 -0400

    Eliminando blog para ver si se resuelve el error en vercel

--- src/app/blog/page.jsx
+++
@@ -1,9 +0,0 @@
-export default function Blog () {
-  return (
-    <div className='blog-main-container'>
-      <div className='blog-title'>
-        <h2>Titulo</h2>
-      </div>
-    </div>
-  )
-}
