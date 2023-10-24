530e77e8c83a959c78c0438590d94915584bb782
Daniel Avila <roicesdaniel@gmail.com>
2023-10-23 20:25:22 -0400

    modificando next.config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true
-  },
-  // images: {
-  //   domains: ['avatars.dicebear.com']
-  // },
   reactStrictMode: true
 }
 
